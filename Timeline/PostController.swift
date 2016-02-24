1e49cdf43f0a5e30c819ebfc1f2977e70b5b7c8a
Brock Chambers <brockchamb@gmail.com>
2016-02-23 17:16:53 -0700

    Finished Part 1

---
+++ Timeline/PostController.swift
@@ -0,0 +1,65 @@
+//
+//  PostController.swift
+//  Timeline
+//
+//  Created by admin on 2/23/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class PostController {
+    
+    static func fetchTimelineForUser(user: User, completion: (posts: [Post]?) -> Void) {
+        completion(posts: mockPosts())
+    }
+    
+    static func addPost(image: UIImage, caption: String?, completion: (success: Bool, post: Post?) -> Void) {
+        completion(success: true, post: mockPosts().first)
+    }
+    
+    static func postFromIdentifier(indentifier: String, completion: (post: Post?) -> Void) {
+        completion(post: mockPosts().first)
+    }
+    
+    static func postsForUser(user: User, completion: (post: [Post]?) -> Void) {
+        completion(post: mockPosts())
+    }
+    
+    static func deletePost(post: Post) {
+        
+    }
+    
+    static func addCommentWithTextPost(string: String, post: Post, completion: (success: Bool, post: Post?) -> Void) {
+        completion(success: true, post: mockPosts().first)
+    }
+    
+    static func deleteComment(comment: Comment, completion: (success: Bool, post: Post?) -> Void) {
+        completion(success: false, post: mockPosts().first)
+    }
+    
+    static func addLikePost(post: Post, completion: (success: Bool, post: Post?) -> Void) {
+        completion(success: true, post: mockPosts().first)
+    }
+    
+    static func deleteLike(like: Like, completion: (success: Bool, post: Post?) -> Void) {
+        completion(success: false, post: mockPosts().first)
+    }
+    
+    static func orderPosts(post: [Post]) -> [Timeline.Post] {
+        
+    }
+    
+    static func mockPosts() -> [Post] {
+        let post1 = Post(imageEndPoint: "K1l4125TYvKMc7rcp5e", caption: "Nice!", username: "Brock", comments: <#T##[Comment]#>, likes: <#T##[Like]#>, identifier: nil)
+        let post2 = Post(imageEndPoint: "K1l4125TYvKMc7rcp5e", caption: nil, username: "Alex", comments: <#T##[Comment]#>, likes: <#T##[Like]#>, identifier: nil)
+        let post3 = Post(imageEndPoint: "K1l4125TYvKMc7rcp5e", caption: nil, username: "SammyB", comments: <#T##[Comment]#>, likes: <#T##[Like]#>, identifier: nil)
+        return [post1, post2, post3]
+    }
+    
+    
+    
+        
+}
---
+++ Timeline/UserController.swift
@@ -0,0 +1,58 @@
+//
+//  UserController.swift
+//  Timeline
+//
+//  Created by admin on 2/23/16.
+//  Copyright © 2016 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+
+class UserController {
+    var currentUser = UserController.mockUsers().first
+    static let sharedController = UserController()
+    
+    static func userForIdentifier(identifier: String, completion: (user: User?) -> Void) {
+        completion(user: mockUsers().first)
+    }
+    
+    static func fetchAllUsers(completion: (user: User) -> Void) {
+        completion(user: mockUsers().first!)
+    }
+    
+    static func followUser(user: User, completion: (success: Bool) -> Void) {
+        completion(success: true)
+    }
+    
+    static func unfollowUser(user: User, completion: (success: Bool) -> Void) {
+        completion(success: true)
+    }
+    
+    static func userFollowsUser(user: User, followsUser: User, completion: (follows: Bool) -> Void) {
+        completion(follows: true)
+    }
+    
+    static func authenticateUser(email: String, password: String, completion: (success: Bool, user: User?) -> Void) {
+        completion(success: true, user: mockUsers().first)
+    }
+    
+    static func createUser(email: String, username: String, password: String, bio: String?, url: String?, completion: (success: Bool, user: User?) -> Void) {
+        completion(success: true, user: mockUsers().first)
+    }
+    
+    static func updateUser(user: User, username: String, bio: String?, url: String?, completion: (success: Bool, user: User?) -> Void) {
+        completion(success: true, user: mockUsers().first)
+    }
+    
+    static func logoutCurrentUser() {
+        
+    }
+    
+    static func mockUsers() -> [User] {
+        let user1 = User(username: "Gibby", bio: "What?", url: nil, identifier: nil)
+        let user2 = User(username: "SammyB", bio: "How much you bench bro?", url: nil, identifier: nil)
+        let user3 = User(username: "Brock", bio: "Hey buddy", url: nil, identifier: nil)
+        return [user1, user2, user3]
+    }
+}
